19b454b742ad9cfe4402f0286187815cee0af9c6
Mtao <mhtcyc@163.com>
2023-05-14 22:20:15 +0800

    导航栏吸顶

--- src/main.js
+++ src/main.js
@@ -7,11 +7,6 @@ import "@/styles/common.scss"
 const app=createApp(App)
 const pinia=createPinia()
 
-import {gerCategory} from "@/apis/testAPI"
-gerCategory().then(res=>{
-    console.log(res);
-})
-
 app.use(router)
 app.use(pinia)
 app.mount('#app')
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,12 +1,14 @@
 <script setup>
 import LayoutNav from './components/LayoutNav.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutFixed from './components/LayoutFixed.vue';
 import LayoutFooter from './components/LayoutFooter.vue';
 import {  } from 'vue-router'
 import {  } from 'vue'
 </script>
 
 <template>
+    <LayoutFixed/>
     <LayoutNav/>
     <LayoutHeader/>
     <RouterView/>
